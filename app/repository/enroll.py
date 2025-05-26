02226cc7988302f89024c4b192303639eab76ff0
bardiafar <bardiafarokhsereshti@gmail.com>
2025-05-26 16:21:53 +0330

    only status 1 for teacher enrolls

--- app/repository/enroll.py
+++ app/repository/enroll.py
@@ -10,7 +10,7 @@ class EnrollRepository(CommonRepository):
 
       
     async def get_by_teacher(self, teacher_id: int):
-        query = f"SELECT enrolls.day,enrolls.time, users.lastname,courses.title FROM enrolls INNER JOIN classes ON enrolls.class_id = classes.id INNER JOIN users ON enrolls.student_id = users.id INNER JOIN courses ON classes.course_id = courses.id WHERE classes.teacher_id = $1;"
+        query = f"SELECT enrolls.day,enrolls.time, users.lastname,courses.title FROM enrolls INNER JOIN classes ON enrolls.class_id = classes.id INNER JOIN users ON enrolls.student_id = users.id INNER JOIN courses ON classes.course_id = courses.id WHERE classes.teacher_id = $1 AND enrolls.status=1;"
         return await self.connection.fetch(query, teacher_id)
 
     async def get_public_by_teacher(self, teacher_id: int):
