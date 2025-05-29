636cacb70f67854fe1d6f81c0be624cf5e2a056b
bardiafar <bardiafarokhsereshti@gmail.com>
2025-05-29 16:23:54 +0330

    added column info to teachers_enroll

--- app/schemas/enroll.py
+++ app/schemas/enroll.py
@@ -118,5 +118,73 @@ class TeachersEnroll(Item):
     student_lastname: str
     course_title: str
 
+
+
+teachers_enroll_column_info = [
+  {
+    "title": "id",
+    "datatype": "int",
+    "description": "ID of the enrollment record"
+  },
+  {
+    "title": "student_id",
+    "datatype": "int",
+    "description": "ID of the associated student"
+  },
+  {
+    "title": "class_id",
+    "datatype": "int",
+    "description": "ID of the associated class"
+  },
+  {
+    "title": "day",
+    "datatype": "int",
+    "description": "Day of the week (0 for Sunday, 6 for Saturday)"
+  },
+  {
+    "title": "time",
+    "datatype": "time",
+    "description": "Time of the enrollment"
+  },
+  {
+    "title": "status",
+    "datatype": "int",
+    "description": "Status of the enrollment (0 for inactive, 1 for active)"
+  },
+  {
+    "title": "credit",
+    "datatype": "int",
+    "description": "Total credits assigned to the enrollment"
+  },
+  {
+    "title": "credit_spent",
+    "datatype": "int",
+    "description": "Credits spent by the enrollment"
+  },
+  {
+    "title": "date_at",
+    "datatype": "datetime",
+    "description": "Date and time when the enrollment was created"
+  },
+  {
+    "title": "student_firstname",
+    "datatype": "str",
+    "description": "First name of the student associated with the enrollment"
+  },
+  {
+    "title": "student_lastname",
+    "datatype": "str",
+    "description": "Last name of the student associated with the enrollment"
+  },
+  {
+    "title": "course_title",
+    "datatype": "str",
+    "description": "Title of the course associated with the enrollment"
+  }
+]
+
+
+
 class GetTeachersEnrollsResponse(BaseModel):
     items: List[TeachersEnroll]
+    column_info: list = teachers_enroll_column_info
